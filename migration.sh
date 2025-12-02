cb5de6242e857bb4693f6a57e078b8ebfe5357bf
codeinu <codeinu@gmail.com>
2025-12-02 22:52:51 +0100

    fix tmux plugin folder + update migration script

--- migration.sh
+++ migration.sh
@@ -1,7 +1,10 @@
 # /bin/bash
 
 cp -R nvim ~/.config/nvim
+
+echo "setting up tmux configuration.."
 cp -R tmux ~/.config/tmux
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 echo "setting up git configuration.."
 git config --global core.editor "nvim"
